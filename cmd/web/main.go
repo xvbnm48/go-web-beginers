85fee2995235cbaf77cd726dff56768c765685f3
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2022-08-16 21:40:58 +0700

    erorr postgres done fix!

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
@@ -8,7 +9,12 @@ import (
 	"time"
 
 	"github.com/CloudyKit/jet/v6"
+	"github.com/alexedwards/scs/postgresstore"
 	"github.com/alexedwards/scs/v2"
+	"github.com/upper/db/v4"
+	"github.com/upper/db/v4/adapter/postgresql"
+
+	_ "github.com/lib/pq"
 )
 
 type application struct {
@@ -34,6 +40,28 @@ func main() {
 		port: "8080",
 		url:  "http://localhost:8080",
 	}
+
+	db2, err := openDB("postgres://fariz:fariz@localhost/hackernews?sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db2.Close()
+
+	// init upperdb
+	upper, err := postgresql.New(db2)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer func(upper db.Session) {
+		err := upper.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(upper)
+
+	// init application
 	app := &application{
 		server:  server,
 		appName: "Hnews",
@@ -57,9 +85,25 @@ func main() {
 	app.session.Cookie.Name = app.appName
 	app.session.Cookie.Domain = app.server.host
 	app.session.Cookie.SameSite = http.SameSiteStrictMode
+	app.session.Store = postgresstore.New(db2)
 
 	if err := app.listenAnServe(); err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println("Hello, world.")
+}
+
+//OpenDB: open database connection
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Golang web for beginners
+
+39.43
+[this refernce](https://youtu.be/VNfOqpD0GIM)
+
+17 agustus! 
